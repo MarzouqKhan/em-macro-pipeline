25b3d06ea5203d911652bca164c1e15b69b94de8
MarzouqKhan <132720931+MarzouqKhan@users.noreply.github.com>
2026-08-04 17:08:46 -0500

    FX API fetch complete, function added for exchange_rate

--- fetch_fx.py
+++ fetch_fx.py
@@ -1,6 +1,21 @@
 import os
 import dotenv
 import requests
+
+# Intialize FX API
 dotenv.load_dotenv()
-fx_rate = 0
-url = ""
+fx_api_key = os.getenv("FX_API_KEY")
+base_url = "https://v6.exchangerate-api.com/v6"
+api_url = f"{base_url}/{fx_api_key}/latest"
+
+# Function that GETs exchange rate
+def exchange_rate(base,target,url):
+    full_url = f"{url}/{base}"
+
+    # GET conversion rate of base-to-target currency
+    r = requests.get(full_url)
+    fx_json_data = r.json()
+    conversion_rate = fx_json_data["conversion_rates"][target]
+    return conversion_rate
+
+# print(exchange_rate("USD","PKR",api_url))
