6a37d5f5cbd0982ebd75951d6c70aa07f677f0de
MarzouqKhan <132720931+MarzouqKhan@users.noreply.github.com>
2026-08-04 17:21:50 -0500

    Added basic ConnectionError handling

--- fetch_fx.py
+++ fetch_fx.py
@@ -11,11 +11,14 @@ api_url = f"{base_url}/{fx_api_key}/latest"
 # Function that GETs exchange rate
 def exchange_rate(base,target,url):
     full_url = f"{url}/{base}"
-
     # GET conversion rate of base-to-target currency
-    r = requests.get(full_url)
+    try:
+        r = requests.get(full_url,timeout=10)
+    except requests.exceptions.ConnectionError:
+        print("Encountered ConnectionError, please ensure you are connected to the Internet.")
+        return None
     fx_json_data = r.json()
     conversion_rate = fx_json_data["conversion_rates"][target]
     return conversion_rate
 
-# print(exchange_rate("USD","PKR",api_url))
+print(exchange_rate("USD","PKR",api_url))
