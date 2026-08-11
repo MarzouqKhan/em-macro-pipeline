6f39a02a6940350c5425cde6730748ae934184db
MarzouqKhan <132720931+MarzouqKhan@users.noreply.github.com>
2026-08-10 21:53:58 -0500

    Wired FX fetcher to allow saving to database

--- fetch_fx.py
+++ fetch_fx.py
@@ -1,6 +1,8 @@
 import os
 import dotenv
 import requests
+import db_manager
+from email.utils import parsedate_to_datetime
 
 # Initialize FX API and .env file
 dotenv.load_dotenv()
@@ -9,7 +11,7 @@ base_url = "https://v6.exchangerate-api.com/v6"
 api_url = f"{base_url}/{fx_api_key}/latest"
 
 # Function that GETs exchange rate
-def exchange_rate(base,target,url):
+def exchange_rate(base,target,url,save=False):
     full_url = f"{url}/{base}"
     # GET conversion rate of base-to-target currency
     try:
@@ -19,6 +21,10 @@ def exchange_rate(base,target,url):
         return None
     fx_json_data = r.json()
     conversion_rate = fx_json_data["conversion_rates"][target]
+    if save:
+        api_time = fx_json_data["time_last_update_utc"]
+        time = parsedate_to_datetime(api_time)
+        db_manager.add_row(f"{base}-{target} Exchange Rate",conversion_rate,time.strftime("%Y-%m-%d"),source="ExchangeRate-API")
     return conversion_rate
 
-# print(exchange_rate("USD","PKR",api_url))
+print(exchange_rate("USD","PKR",api_url,True))
